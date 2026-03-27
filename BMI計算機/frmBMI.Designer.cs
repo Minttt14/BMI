1faa160ede073cf54ab5c697486a2bff8a1bc4dd
Minttt14 <Xuan.cyo26@gmail.com>
2026-03-27 22:56:56 +0800

    加入專案檔案。

---
+++ BMI計算機/frmBMI.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace BMI計算機
+{
+    partial class frmBMI
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblBFPResult = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblBMIResult = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdoFemale = new System.Windows.Forms.RadioButton();
+            this.rdoMale = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblBFPResult);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.lblBMIResult);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.groupBox2.Location = new System.Drawing.Point(81, 407);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(816, 213);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "計算結果";
+            // 
+            // lblBFPResult
+            // 
+            this.lblBFPResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblBFPResult.Location = new System.Drawing.Point(422, 137);
+            this.lblBFPResult.Name = "lblBFPResult";
+            this.lblBFPResult.Size = new System.Drawing.Size(300, 40);
+            this.lblBFPResult.TabIndex = 8;
+            this.lblBFPResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.Location = new System.Drawing.Point(69, 134);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(229, 40);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "體脂肪率 (BFP)";
+            // 
+            // lblBMIResult
+            // 
+            this.lblBMIResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblBMIResult.Location = new System.Drawing.Point(422, 63);
+            this.lblBMIResult.Name = "lblBMIResult";
+            this.lblBMIResult.Size = new System.Drawing.Size(300, 40);
+            this.lblBMIResult.TabIndex = 6;
+            this.lblBMIResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(69, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(289, 40);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "身體質量指數(BMI)";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdoFemale);
+            this.groupBox1.Controls.Add(this.rdoMale);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtAge);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.btnRun);
+            this.groupBox1.Controls.Add(this.txtWeight);
+            this.groupBox1.Controls.Add(this.txtHeight);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.groupBox1.Location = new System.Drawing.Point(81, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(816, 326);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "輸入";
+            // 
+            // rdoFemale
+            // 
+            this.rdoFemale.AutoSize = true;
+            this.rdoFemale.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.rdoFemale.Location = new System.Drawing.Point(398, 255);
+            this.rdoFemale.Name = "rdoFemale";
+            this.rdoFemale.Size = new System.Drawing.Size(80, 44);
+            this.rdoFemale.TabIndex = 9;
+            this.rdoFemale.TabStop = true;
+            this.rdoFemale.Text = "女";
+            this.rdoFemale.UseVisualStyleBackColor = true;
+            // 
+            // rdoMale
+            // 
+            this.rdoMale.AutoSize = true;
+            this.rdoMale.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.rdoMale.Location = new System.Drawing.Point(305, 257);
+            this.rdoMale.Name = "rdoMale";
+            this.rdoMale.Size = new System.Drawing.Size(80, 44);
+            this.rdoMale.TabIndex = 8;
+            this.rdoMale.TabStop = true;
+            this.rdoMale.Text = "男";
+            this.rdoMale.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(78, 258);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 40);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "性別";
+            // 
+            // txtAge
+            // 
+            this.txtAge.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtAge.Location = new System.Drawing.Point(305, 191);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(173, 39);
+            this.txtAge.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(78, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 40);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "年齡";
+            // 
+            // btnRun
+            // 
+            this.btnRun.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnRun.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnRun.Location = new System.Drawing.Point(629, 128);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(113, 93);
+            this.btnRun.TabIndex = 4;
+            this.btnRun.Text = "計算";
+            this.btnRun.UseVisualStyleBackColor = false;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtWeight.Location = new System.Drawing.Point(305, 123);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(173, 39);
+            this.txtWeight.TabIndex = 3;
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtHeight.Location = new System.Drawing.Point(305, 55);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(173, 39);
+            this.txtHeight.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(78, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 40);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "體重(kg)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(78, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "身高(cm)";
+            // 
+            // frmBMI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(979, 673);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmBMI";
+            this.Text = "BMI計算機";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblBFPResult;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblBMIResult;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdoFemale;
+        private System.Windows.Forms.RadioButton rdoMale;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
